c39e76419a9eb3f9f5287e7f5eeee4ca3e39b432
hyunwoo <dlagusdn0218@gmail.com>
2025-05-18 02:22:27 +0900

    Fix : Booth id 값 다른 모델들과 통일

--- booth/models.py
+++ booth/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Booth(models.Model):
-    # id = models.AutoField(primary_key=True) # 자동 생성 되므로 생략
+    id = models.AutoField(primary_key=True) # 자동 생성 되므로 생략
     name = models.CharField(max_length=30)
     total_revenues = models.IntegerField(default=0)
 
