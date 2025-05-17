0032117cd5f0bb2381b7c5139efa600e64daf402
ParkSuhhyun <2022111511@dgu.ac.kr>
2025-05-18 01:37:47 +0900

    Feat: 모델 생성

--- booth/models.py
+++ booth/models.py
@@ -6,3 +6,12 @@ class Booth(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
     total_revenues = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+    
+class Table(models.Model):
+    id = models.AutoField(primary_key=True)
+    booth_id = models.ForeignKey('Booth', on_delete=models.CASCADE)
+    table_num = models.IntegerField()
+    table_status = models.CharField(max_length=10)
