9a9a2e4e6d782072d2789f0af8d76128f6d2c2a5
lsoobin <vini20012203@gmail.com>
2025-05-24 14:57:06 +0900

    fix: str 메서드 추가 및 admin 설정

--- order/admin.py
+++ order/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Menu)
-admin.site.register(Order)
-admin.site.register(Cart)
 
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
