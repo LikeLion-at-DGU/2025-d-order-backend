e999b9a07140ef7044a81a2f5089607a987b851e
soobinLIM <163871529+lsoobin@users.noreply.github.com>
2025-05-24 15:34:21 +0900

    Update admin.py
    admin용으로 수정

--- booth/admin.py
+++ booth/admin.py
@@ -9,6 +9,6 @@ class TableAdmin(admin.ModelAdmin):
     list_display = ('id', 'table_num', 'booth_name')
 
     def booth_name(self, obj):
-        return obj.booth.name
+        return obj.booth_id.name
     booth_name.short_description = 'Booth Name'
 
