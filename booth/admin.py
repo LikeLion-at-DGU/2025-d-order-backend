4c5494fc9bae0316491e9419d585598c8c909b7b
soobinLIM <163871529+lsoobin@users.noreply.github.com>
2025-05-27 12:28:12 +0900

    chore fix: admin.py 수정 부스 총매출 띄우기

--- booth/admin.py
+++ booth/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import *
+from order.models import Order
+from django.db.models import Sum, F
 
 
-admin.site.register(Booth)
 
 @admin.register(Table)
 class TableAdmin(admin.ModelAdmin):
@@ -12,3 +13,19 @@ class TableAdmin(admin.ModelAdmin):
         return obj.booth_id.name
     booth_name.short_description = 'Booth Name'
 
+class BoothAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'total_revenue_display', 'qr_code_image') 
+
+    def total_revenue_display(self, obj):
+        total = Order.objects.filter(
+            menu_id__booth_id=obj.id,
+            order_status__in=['order_complete', 'served_complete']
+        ).aggregate(
+            total=Sum(F('menu_num') * F('menu_id__menu_price'))
+        )['total'] or 0
+
+        return f"{total:,}원" 
+
+    total_revenue_display.short_description = '총 매출'
+
+admin.site.register(Booth, BoothAdmin)
